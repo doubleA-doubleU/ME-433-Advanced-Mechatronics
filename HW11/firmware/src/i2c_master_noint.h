bca85877f45a56c8869425dd387aed8704a70372
weatherman03 <aaron.m.weatherly@gmail.com>
2017-05-02 18:41:45 -0500

    HW11

---
+++ HW11/firmware/src/i2c_master_noint.h
@@ -0,0 +1,24 @@
+#ifndef I2C_MASTER_NOINT_H__
+#define I2C_MASTER_NOINT_H__
+// Header file for i2c_master_noint.c
+// helps implement use I2C2 as a master without using interrupts
+
+#define SLAVE_ADDR 0x20             // MCP23008 address
+#define IODIR 0x00                  // address for input/output direction register
+#define GPIO 0x09                   // address for the read register
+#define OLAT 0x0A                   // address for the write register
+
+void i2c_master_setup(void);              // set up I2C2 as a master, at 100 kHz
+
+void i2c_master_start(void);              // send a START signal
+void i2c_master_restart(void);            // send a RESTART signal
+void i2c_master_send(unsigned char byte); // send a byte (either an address or data)
+unsigned char i2c_master_recv(void);      // receive a byte of data
+void i2c_master_ack(int val);             // send an ACK (0) or NACK (1)
+void i2c_master_stop(void);               // send a stop
+
+void initExpander(void);  // initialize MCP23008 pin expander
+void setExpander(unsigned char pin, unsigned char level); // set an output pin high or low
+unsigned char getExpander(void); // read from an input pin
+
+#endif
